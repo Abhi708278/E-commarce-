c32273817f001182e17a8d3dac5351edd50b8c7a
Abhi708278 <abhi708278@gmail.com>
2025-01-11 13:26:22 +0530

    Added db connection

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const itemRoutes = require("./src/routes/itemRoutes");
 const categoryRoutes = require("./src/routes/CategoryRoutes"); // Import category routes
 //const connectDB = require("./src/config/connectDB");
+const connection = require('./src/db');
 const cors = require("cors");
 
 const app = express();
@@ -24,6 +26,17 @@ app.use(express.json()); // This is necessary to parse JSON bodies
 // Use routes for items and categories
 app.use("/items", itemRoutes); // Item routes
 app.use("/categories", categoryRoutes); // Category routes
+
+// test api
+app.get('/test',(req,res)=>{
+  res.json({success:true,data:{
+
+    mainData:[1,23,4],
+    message:"Hey It's a test data"
+
+  }})
+
+});
 
 // Start the server
 app.listen(port, () => {
